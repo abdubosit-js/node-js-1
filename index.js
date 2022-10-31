22be2834389c6ce490bc3e4f5b31532bd4797924
abdubosit-js <abdubositjs@gmail.com>
2022-10-31 17:15:53 +0500

    ikkinchi commit

--- index.js
+++ index.js
@@ -1,33 +1,68 @@
-const user = {id: 1, name: "abdubosit"}
 
-const friends = [{id: 2, name: 'friends1'}, {id: 3, name: "friend2",}]
+// const user = {id: 1, name: "abdubosit"}
 
-const getUser = () => {
-    return new Promise((res, rej) => {
-        setTimeout(() => {
-            res(user)
-        }, 2000)
+// const friends = [{id: 2, name: 'friends1'}, {id: 3, name: "friend2",}]
+
+// const getUser = () => {
+//     return new Promise((res, rej) => {
+//         setTimeout(() => {
+//             res(user)
+//         }, 2000)
+//     })
+// }
+
+// const getFriends = (userId) => {
+//     return new Promise((res, rej) => {
+//         setTimeout(() => {
+//             res(friends)
+//         }, 1000)
+//     })
+// }
+// let userId;
+
+// --callback hell
+
+/* getUser()
+    .then((user) => {
+        userId = user.id
+        getFriends(userId)
+            .then((friends) => {
+                console.log(user)
+                console.log(friends)
+            })
+    }) */
+
+// --promise Chain
+
+/* getUser()
+    .then((user) => {
+        return getFriends(user.id)
     })
-}
-
-const getFriends = (userId) => {
-    return new Promise((res, rej) => {
-        setTimeout(() => {
-            res(friends)
-        }, 1000)
-    })
-}
-
-// callback hell
-
-let userId;
-getUser().then((user) => {
-    userId = user.id
-    getFriends(userId).then((friends) => {
+    .then((friends) => {
         console.log(user)
         console.log(friends)
-    })
-})
+    }) */
 
-// -- promise Chain
+// async||await
 
+/* async function asyncFunction() {
+    try {
+        const user = await getUser()
+        const friends = await getFriends()
+        console.log(user, friends)
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+asyncFunction() */
+
+// --require qilish
+
+// const { bir, ikki } = require("./modul")
+// console.log(bir(),ikki())
+
+
+const slugfy = require('slugify')
+
+console.log(slugfy("some string", "_"))
---
+++ modul.js
@@ -0,0 +1,5 @@
+const bir = () => "bir"
+const ikki = () => "ikki"
+
+module.exports.bir = bir;
+module.exports.ikki = ikki;
